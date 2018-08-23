f5fea6d6476dad807a08874070fc14a4897dfb3f
Rafael Telles <raftel.ti@gmail.com>
2018-08-23 08:50:20 -0300

    Add Github authentication

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-from flask import Flask
-app = Flask(__name__)
+import os
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+from flask import Flask, redirect, url_for
+from werkzeug.contrib.fixers import ProxyFix
+from flask_dance.contrib.github import make_github_blueprint, github
+
+app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app)
+app.secret_key = os.getenv("APP_SECRETKEY")
+blueprint = make_github_blueprint(
+    client_id=os.getenv("GITHUB_CLIENT_ID"),
+    client_secret=os.getenv("GITHUB_CLIENT_SECRET"),
+)
+app.register_blueprint(blueprint, url_prefix="/login")
+
+@app.route("/")
+def index():
+    if not github.authorized:
+        return redirect(url_for("github.login"))
+    resp = github.get("/user")
+    assert resp.ok
+    return "You are @{login} on GitHub".format(login=resp.json()["login"])
+
+if __name__ == "__main__":
+    app.run()

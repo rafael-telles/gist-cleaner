d3446a159b55878819c2bc7115e9cfcfa5e0c04d
Rafael Telles <raftel.ti@gmail.com>
2018-08-23 09:00:16 -0300

    Try to use Flask-Github

--- main.py
+++ main.py
@@ -1,25 +1,49 @@
 import os
 
-from flask import Flask, redirect, url_for
-from werkzeug.contrib.fixers import ProxyFix
-from flask_dance.contrib.github import make_github_blueprint, github
+from flask import Flask
+from flask_github import GitHub
 
 app = Flask(__name__)
-app.wsgi_app = ProxyFix(app.wsgi_app)
-app.secret_key = os.getenv("APP_SECRETKEY")
-blueprint = make_github_blueprint(
-    client_id=os.getenv("GITHUB_CLIENT_ID"),
-    client_secret=os.getenv("GITHUB_CLIENT_SECRET"),
-)
-app.register_blueprint(blueprint, url_prefix="/login")
+app.config['GITHUB_CLIENT_ID'] = os.getenv("GITHUB_CLIENT_ID")
+app.config['GITHUB_CLIENT_SECRET'] = os.getenv("GITHUB_CLIENT_SECRET")
+
+github = GitHub(app)
+
+
+@github.access_token_getter
+def token_getter():
+    user = g.user
+    if user is not None:
+        return user.github_access_token
+
+
+@app.route('/github-callback')
+@github.authorized_handler
+def authorized(oauth_token):
+    next_url = request.args.get('next') or url_for('index')
+    if oauth_token is None:
+        flash("Authorization failed.")
+        return redirect(next_url)
+
+    user = User.query.filter_by(github_access_token=oauth_token).first()
+    if user is None:
+        user = User(oauth_token)
+        db_session.add(user)
+
+    user.github_access_token = oauth_token
+    db_session.commit()
+    return redirect(next_url)
+
 
 @app.route("/")
 def index():
-    if not github.authorized:
-        return redirect(url_for("github.login"))
-    resp = github.get("/user")
-    assert resp.ok
-    return "You are @{login} on GitHub".format(login=resp.json()["login"])
+    if not github.user:
+        return github.authorize()
+
+    return "You are @{login} on GitHub".format(login=dict(github.user))
+
+
+
 
 if __name__ == "__main__":
     app.run()
